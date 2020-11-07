1340f1444cd8e850fb5034ddac1a2b914a3951d2
explorerjs32 <explorerjs32@gmail.com>
2020-11-07 02:30:53 -0500

    Added portion to do apperture photometry.

--- photometry_hw.py
+++ photometry_hw.py
@@ -1,7 +1,13 @@
-import numpy as np
 from astropy.io import fits
+from astropy.modeling import models, fitting
+from astropy.stats import sigma_clipped_stats, mad_std
 from astropy.visualization import ZScaleInterval, ImageNormalize
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+from photutils import aperture_photometry, CircularAperture, CircularAnnulus
+from photutils import datasets
+from photutils import DAOStarFinder, find_peaks
 from skimage.feature import *
 from scipy.ndimage import interpolation as interp
 import os
@@ -67,7 +73,7 @@ def Align_Image(images):
             target_image = images[i]
 
             # Calculate the shift of the images
-            result, error, diffphase = register_translation(template_image, target_image, 1000)
+            result, error, diffphase = register_translation(template_image, target_image, 5000)
 
             # Align the images based on the shift
             align_image = interp.shift(target_image, result)
@@ -78,6 +84,79 @@ def Align_Image(images):
     aligned_images.append(template_image)
 
     return aligned_images
+
+def Sources_Positions(x_peaks, y_peaks, values):
+    p = np.zeros((len(x_peaks), 3))
+
+    for i in range(len(x_peaks)):
+        p[i,0] = x_peaks[i]
+        p[i,1] = y_peaks[i]
+        p[i,2] = values[i]
+
+    return p
+
+def Peak_Positions(data, upper_lim, lower_lim):
+    mean, median, std = sigma_clipped_stats(data, sigma=3.)
+    treshold = median + (3.*std)
+    peaks_table = find_peaks(data, treshold, box_size=11.)
+    
+    positions = Sources_Positions(peaks_table['x_peak'], peaks_table['y_peak'], peaks_table['peak_value'])
+
+    x = []
+    y = []
+    val = []
+
+    for i in range(len(positions[:,0])):
+
+        if positions[:,2][i] > lower_lim and positions[:,2][i] < upper_lim:
+            x.append(positions[:,0][i])
+            y.append(positions[:,1][i])
+            val.append(positions[:,2][i])
+          
+    return np.array(x), np.array(y), np.array(val)
+
+def Gauss_Fit(data):
+    # Convert the image to 1D
+    data_1d = np.sum(data, axis=0)
+    xpix = np.arange(data_1d.size)
+        
+    # Perform the Gaussian Fit
+    t_init = models.Gaussian1D()
+    fit_t = fitting.LevMarLSQFitter()
+    t = fit_t(t_init, xpix, data_1d)
+
+    x_fit = np.linspace(xpix[0], xpix[-1], 1000)
+
+    # Plot the Gauss fit
+    '''
+    plt.title('Gaussian Fit')
+    plt.plot(xpix, data_1d, 'ro', label='Star')
+    plt.plot(x_fit, t(x_fit), color='black', label='Gaussian Fit')
+    plt.legend()
+    plt.show()
+    '''
+    return t
+
+def Photometry(data, star_an, sky_an, et):
+    # Measure the brightness of the star and the sky
+    star_phot = aperture_photometry(data, star_an)
+    sky_phot = aperture_photometry(data, sky_an)
+
+    # Calculate the mean sky brightness and subtract it from the star
+    sky_mean = sky_phot['aperture_sum']/sky_an.area
+    sky_sum = sky_mean*star_an.area
+    star_tot_brightness = star_phot['aperture_sum'] - sky_sum
+
+    # Measure the flux and its error (Use the gain form the CCD Lab)
+    gain = 0.0778
+    flux = (star_tot_brightness*gain)/et
+    flux_err = np.sqrt(flux)
+
+    # Measure the Instrumental magnitude and its error
+    inst_mag = -2.5*np.log10(flux)
+    inst_mag_err = (2.5/np.log(10))*(flux_err/flux)
+
+    return star_tot_brightness, flux, flux_err, inst_mag, inst_mag_err
 
 
 date = '2020-10-17'
@@ -342,10 +421,97 @@ for filt in filters:
     else:
         pass
 '''
-    
+# Measure the FWHM, PSF photometry, and appreture photometry
+data_dir = main_dir+'/reduced_files/'
+data_files = os.listdir(data_dir)
 
-    
-    
+for file in data_files:
+    # Analyze only the science frames
+    fr = fits.getheader(data_dir+file)['FRAME']
+
+    # Extract the information of the header
+    if fr == 'Light':
+        obj = fits.getheader(data_dir+file)['OBJECT']
+        et = fits.getheader(data_dir+file)['EXPTIME']
+        f = fits.getheader(data_dir+file)['FILTER']
+
+        # Extract the image data and find the ADU peaks
+        data = fits.getdata(data_dir+file).astype(float)
+
+        upper_lim = 70000.
+        lower_lim = 20000.
+        
+        x, y, val = Peak_Positions(data, upper_lim, lower_lim)
+        print('\n', obj, f, '- Peaks found:', len(x) )
+        
+        for i in range(len(x)):
+            # Decide if the identified star should be measured or not
+            norm = ImageNormalize(data, interval=ZScaleInterval())
+            '''
+            plt.title(obj+' '+f+' Filter')
+            plt.imshow(data, cmap='gray', origin='lower', norm=norm)
+            plt.plot(x[i], y[i], '+', c='darkblue', ms=20)
+            plt.text(x[i]+20, y[i]-50, str(i+1), c='darkblue') 
+            plt.show()
+            '''
+            measure = input('\nDo you wish to measure this star(y/n):')
+
+            if measure == 'n':
+                print('Peak', i+1, 'ignored')
+                pass
+
+            else:
+                print('Measuring peak', i+1)
+
+                # Crop the image to see the star only
+                croped_data = data[int(y[i])-15:int(y[i])+15, int(x[i])-15:int(x[i])+15] 
+                '''
+                norm = ImageNormalize(croped_data, interval=ZScaleInterval())
+                
+                plt.title(obj+' '+f+' Peak Value: '+str(val[i]))
+                plt.imshow(croped_data, cmap='gray', origin='lower', norm=norm)
+                plt.show()
+                '''
+                # Measure the FWHM of the star
+                fwhm = Gauss_Fit(croped_data).stddev[0]
+                print('- The FWHM is', round(fwhm, 4), 'pixels')
+
+                # Define the appertures for the annuli
+                # Main Annulus
+                star_an = CircularAperture((x[i], y[i]), r=1.3*fwhm)
+
+                # Sky positions - Slect an area of the image where the are no stars
+                sky_pos = (1400., 500.)
+                sky_an = CircularAnnulus(sky_pos, r_in=2.*fwhm, r_out=3.*fwhm)
+                
+                plt.title(obj+' '+f)
+                plt.imshow(data, cmap='gray', origin='lower', norm=norm)
+                star_an.plot(color='yellow')
+                sky_an.plot(color='orange')
+                plt.show()
+                
+                # Measure star's total brightnes, flux, and instrumental magnitude
+                star_tot_brightness, flux, flux_err, inst_mag, inst_mag_err = Photometry(data, star_an, sky_an, et)
+
+                print('- The total star brightnes is', round(float(star_tot_brightness), 4,),
+                      '\n- The star flux is', round(float(flux), 4), '+/-', round(float(flux_err), 1),
+                      '\n- The star instrumental magnitude is', round(float(inst_mag), 4), '+/-', round(float(inst_mag_err), 6))
+
+                    
+                
+
+                    
+                    
+
+                
+              
+                
+        
+
+        
+
+
+
     
         
 
